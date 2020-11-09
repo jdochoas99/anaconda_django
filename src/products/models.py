7ffef53be636cbd3e2c3cf4651782849ff0c4890
Juan David Ochoa Saavedra <juan.david@sefin.ro.gov.br>
2020-11-09 11:45:58 -0400

    created class purchase

--- src/products/models.py
+++ src/products/models.py
@@ -1,8 +1,25 @@
 from django.db import models
-
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import ForeignKey
+from django.contrib.auth.models import User
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=220)
     date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
        return f"{self.name}"
+
+class Purchase(models.Model):
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+    total_price = models.PositiveIntegerField(blank=True)
+    salesman = models.ForeignKey(User, on_delete=CASCADE)
+    #Change required
+    date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.price * self.quantity
+        super().save(*args, **kwargs)
+    def __str__(self):
+       return f"solled to {self.product.name} - {self.quantity} items for {self.price}"
