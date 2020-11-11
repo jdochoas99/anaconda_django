940625438e800e72b658293da2c91e1963a83057
Juan David Ochoa Saavedra <juan.david@sefin.ro.gov.br>
2020-11-11 11:34:56 -0400

    Adding static files

--- src/products/views.py
+++ src/products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from pandas.core.reshape.merge import merge
+from django.contrib.auth.models import User
 from products.models import Product, Purchase
 import pandas as pd
 # Create your views here.
@@ -6,7 +8,7 @@ def chart_select_view(request):
     product_df = pd.DataFrame(Product.objects.all().values())
     product_df['product_id']= product_df['id']
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
-    df = pd.merge(purchase_df,product_df, on='product_id').drop(['id_y','date_y'],axis=1).rename({'id_x': 'id', 'date_x':'date'},axis=1)
+    df = pd.merge(purchase_df,product_df, on='product_id',).drop(['id_y','date_y'],axis=1).rename({'id_x': 'id', 'date_x':'date'},axis=1)
     print(product_df)
     context={
         'df': df.to_html(),
