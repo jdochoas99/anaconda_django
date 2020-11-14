5fcf6f021b165cc0327df51625029d13cc46144e
Juan David Ochoa Saavedra <jdochoas99@gmail.com>
2020-11-14 12:21:58 -0400

    Sending the data to the view

--- src/products/views.py
+++ src/products/views.py
@@ -4,15 +4,25 @@ from django.contrib.auth.models import User
 from products.models import Product, Purchase
 import pandas as pd
 # Create your views here.
+
+
 def chart_select_view(request):
     product_df = pd.DataFrame(Product.objects.all().values())
-    product_df['product_id']= product_df['id']
+    product_df['product_id'] = product_df['id']
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
-    df = pd.merge(purchase_df,product_df, on='product_id',).drop(['id_y','date_y'],axis=1).rename({'id_x': 'id', 'date_x':'date'},axis=1)
-    print(product_df)
-    context={
+    df = pd.merge(purchase_df, product_df, on='product_id',).drop(
+        ['id_y', 'date_y'], axis=1).rename({'id_x': 'id', 'date_x': 'date'}, axis=1)
+    if request.method == 'POST':
+        print(request.POST)
+        chart_type = request.POST['sales']
+        date_from = request.POST['date_from']
+        date_to = request.POST['date_to']
+        print(chart_type)
+        print(date_from, date_to)
+    # print(product_df)
+    context = {
         'df': df.to_html(),
-        'products' : product_df.to_html(),
-        'purchases' : purchase_df.to_html(),
+        'products': product_df.to_html(),
+        'purchases': purchase_df.to_html(),
     }
-    return render(request, 'products/main.html',context)
+    return render(request, 'products/main.html', context)
